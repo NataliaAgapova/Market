1002cc7c721a2734527efb4eed6532c96f25f7ad
Marietta <spitterly@gmail.com>
2017-05-23 20:22:09 +0300

    stop autoplay

--- js/site.js
+++ js/site.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
 
 $('.slider').slick({
-	autoplay: true,
-	autoplaySpeed: 2000,
 	dots: true,
 	fade: true,
 	pauseOnHover: false
@@ -12,7 +10,6 @@ $(document).on("click", ".item img", function() {
 
     $('.slider').slick('slickGoTo',(this.id-1), false);
     $('#open_modal').toggle();
-    $('.slider').slick('slickPlay');
 });
 
   $(document).on("click", ".close", function() {
